4bc0e33657d1ad8ecb87188dcb753d6197c2d138
duylunlxag <duylunlxag@gmail.com>
2023-03-21 23:45:43 +0700

    Read DB

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,7 +1,12 @@
+const Course = require('../models/Course');
+
 class SiteController {
     // [GET] /
-    index(req, res) {
-        res.render('home');
+    index(req, res, next) {
+        Course.find({})
+            .lean()
+            .then((courses) => res.render('home', { courses }))
+            .catch(next);
     }
 
     // [GET] /search
